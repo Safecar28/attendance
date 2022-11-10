2b380c25405aa641344dabf3a2c04f414196bdc6
Safecar28 <nealnpatelthegreat@gmail.com>
2022-11-10 20:58:35 +0530

    simplify code

--- lib/screens/student_page.dart
+++ lib/screens/student_page.dart
@@ -4,11 +4,11 @@ class StudentPage extends StatefulWidget {
   const StudentPage({
     Key? key,
     required this.homeroomId,
-    required this.allStudents,
+    required this.data,
   }) : super(key: key);
 
   final String homeroomId;
-  final Iterable<Student> allStudents;
+  final Iterable<Student> data;
 
   @override
   createState() => _StudentPageState();
@@ -30,7 +30,7 @@ class _StudentPageState extends State<StudentPage> {
         builder: (context, snapshot) {
           if (!snapshot.hasData) return const CircularProgressIndicator();
           var h = snapshot.data as Homeroom;
-          var students = widget.allStudents
+          var students = widget.data
               .where((std) => h.studentIds.any((id) => id == std.id));
           return Scaffold(
               appBar: AppBar(
--- lib/widgets/homeroom_list.dart
+++ lib/widgets/homeroom_list.dart
@@ -3,37 +3,32 @@ part of '../main.dart';
 class HomeroomList extends StatelessWidget {
   const HomeroomList({
     Key? key,
-    required Iterable<Homeroom> homerooms,
-    required Future<Iterable<Student>> students,
-  })  : _homerooms = homerooms,
-        _students = students,
-        super(key: key);
+    required this.homerooms,
+    required this.students,
+  }) : super(key: key);
 
-  final Iterable<Homeroom> _homerooms;
-  final Future<Iterable<Student>> _students;
+  final Iterable<Homeroom> homerooms;
+  final Future<Iterable<Student>> students;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Iterable<Student>>(
-        future: _students,
+        future: students,
         builder: ((context, snapshot) {
           if (!snapshot.hasData) return const Wait();
 
           return ListView.builder(
-              itemCount: _homerooms.length,
+              itemCount: homerooms.length,
               itemBuilder: (context, index) {
-                final homeroom = _homerooms.elementAt(index);
+                final h = homerooms.elementAt(index);
+                final sp = StudentPage(homeroomId: h.id, data: snapshot.data!);
+                // return list
                 return ListTile(
-                  title: Text(homeroom.name),
-                  subtitle: Text("${homeroom.studentIds.length} students"),
+                  title: Text(h.name),
+                  subtitle: Text("${h.studentIds.length} students"),
                   onTap: () {
                     Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => StudentPage(
-                                  homeroomId: homeroom.id,
-                                  allStudents: snapshot.data!,
-                                )));
+                        context, MaterialPageRoute(builder: (context) => sp));
                   },
                 );
               });
